c033cc113e3ab9f36363b6a568e6050932842054
Szabolcs Besenyei <besenyei.szabolcs@nyfbox.hu>
2017-06-30 19:56:35 +0200

    recursive binary search, and readme fix

--- BinarySearch/BinarySearch.kt
+++ BinarySearch/BinarySearch.kt
@@ -1,15 +1,17 @@
+import java.util.*
+
 /**
  * Created by gazollajunior on 03/04/16.
  */
 
-fun <T:Comparable<T>>binarySearch(list:List<T>, key:T):Int?{
+fun <T: Comparable<T>> binarySearch(list: List<T>, key: T): Int? {
     var rangeStart = 0
     var rangeEnd = list.count()
     while (rangeStart < rangeEnd) {
         val midIndex = rangeStart + (rangeEnd - rangeStart)/2
         if (list[midIndex] == key) {
             return midIndex
-        } else if (list[midIndex] < key){
+        } else if (list[midIndex] < key) {
             rangeStart = midIndex + 1
         } else {
             rangeEnd = midIndex
@@ -18,12 +20,30 @@ fun <T:Comparable<T>>binarySearch(list:List<T>, key:T):Int?{
     return null
 }
 
+fun <T: Comparable<T>> binarySearchRec(list: List<T>, key: T): Optional<Int> {
+    require(list == list.sorted())
+
+    fun helper(start: Int, end: Int): Optional<Int> {
+        val mid: Int = start + (end - start) / 2
+        if (end < start) return Optional.empty()
+
+        if (key == list[mid]) {
+            return Optional.of(mid)
+        } else if (key < list[mid]) {
+            return helper(start, mid - 1)
+        } else {
+            return helper(mid + 1, end)
+        }
+    }
+
+    return helper(0, list.count())
+}
 
 fun main(args: Array<String>) {
     println("\nOrdered list:")
-    val ordered = listOf<String>("Adam", "Clark", "John", "Tim", "Zack")
+    val ordered = listOf("Adam", "Clark", "John", "Tim", "Zack")
     println(ordered)
     val name = "John"
-    val position = binarySearch(ordered, name)
-    println("\n${name} is in the position ${position} in the ordered List.")
+    println("\n$name is in the position ${binarySearch(ordered, name)} in the ordered List.")
+    println("\n$name is in the position ${binarySearchRec(ordered, name)} in the ordered List.")
 }

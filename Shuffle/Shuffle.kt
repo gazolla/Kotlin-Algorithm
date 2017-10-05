79a25ba7ed5b8b46a4e0466f7295e5b053c1289d
Luís Soares <lsoares@gmail.com>
2017-10-05 12:09:04 +0100

    Update Shuffle.kt

--- Shuffle/Shuffle.kt
+++ Shuffle/Shuffle.kt
@@ -15,9 +15,12 @@ fun <T:Comparable<T>>shuffle(items:MutableList<T>):List<T>{
     return items
 }
 
-
-
-
+/* extension version */
+fun <T> Iterable<T>.shuffle(): List<T> {
+    val list = this.toMutableList().apply {  }
+    Collections.shuffle(list)
+    return list
+}
 
 fun main(args: Array<String>) {
     println("\nOrdered list:")
@@ -26,4 +29,7 @@ fun main(args: Array<String>) {
     println("\nshuffled list:")
     val shuffled = shuffle(ordered as MutableList<String>)
     print(shuffled)
+    
+    val orderedB = listOf(1, 2, 3, 4, 5)
+    print(orderedB.shuffle())
 }
